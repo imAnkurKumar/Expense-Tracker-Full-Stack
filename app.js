abd5d197ecd07207893f5f51ff7ff1c33cfa3982
imAnkurKumar <imankur.ak@gmail.com>
2024-04-07 14:43:38 +0530

    Razorpay integration

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const userRouter = require("./router/userRouter");
 const sequelize = require("./util/database");
 const expenseRouter = require("./router/expenseRouter");
+const purchaseRouter = require("./router/purhaseMembershipRouter");
 const User = require("./models/userModel");
 const Expense = require("./models/expenseModel");
+const Order = require("./models/orders");
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -20,9 +24,13 @@ app.use("/user/", userRouter);
 
 app.use("/", expenseRouter);
 app.use("/expense", expenseRouter);
+app.use("/purchase", purchaseRouter);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize
   .sync()
